9dd86587284535e1b3a9f2d07bb7978ffaa20c41
gabrielnav <gabrielnav.j@gmail.com>
2022-10-06 20:19:42 -0300

    Lista de alumno

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+ 
+  alumno: alumno[] = [
+    {
+      nombre: "Luis",
+      apellido: "Garzon",
+      nota: 8
+    },
+    {
+      nombre: "Mateo",
+      apellido: "Martinez",
+      nota: 10
+    },
+    {
+      nombre: "Irina",
+      apellido: "Castro",
+      nota: 8
+    }
+  ]
 
   ngOnInit(): void {
   }
